289dcdb25c5993b8438355bbee0b496a36c2636d
Pretin <hugodrigues99@gmail.com>
2026-06-03 18:02:28 -0300

    Victor Hugo | index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const aparecerDepois = 10000; // 10s
+const tempoNaTela = 5000; // 5s
 
+const video = document.getElementById("jumpscare");
+
+video.style.display = "none";
+
+setTimeout(() => {
+    video.style.display = "block";
+    video.play();
+
+    setTimeout(() => {
+        video.style.display = "none";
+        video.pause();
+    }, tempoNaTela);
+
+}, aparecerDepois);
+
+function iniciarAnimacao(){
+
+    const video = document.getElementById("animacao");
+
+    video.style.display = "block";
+
+    video.play();
+
+    video.onended = () => {
+
+        window.location.href = "https://seusite.com";
+
+    };
+
+}
