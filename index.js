d8442235fcb0cb7a78e29c75dd3a947ac900566d
Pretin <hugodrigues99@gmail.com>
2026-06-10 14:31:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-function iniciarAnimacao(){
-
-    const video = document.getElementById("animacao");
-
-    video.style.display = "block";
-
-    video.play();
-
-    video.onended = () => {
-        window.location.href = "https://seusite.com";
-    };
-}
-
 const titulo = document.getElementById("easterEgg");
 const videoSecreto = document.getElementById("videoSecreto");
 
 let cliques = 0;
 
 titulo.addEventListener("click", () => {
-
     cliques++;
 
-    if(cliques === 5){
-
+    if (cliques === 5) {
         videoSecreto.style.display = "block";
         videoSecreto.play();
 
@@ -33,6 +18,21 @@ titulo.addEventListener("click", () => {
     }
 });
 
-function abrirPagina(pagina) {
-    window.location.href = pagina;
-}
+const youtubeURL = "https://www.youtube.com/watch?v=8KxkzngOGJY";
+
+document.getElementById("faca").addEventListener("click", () => {
+
+    const videoId = youtubeURL.includes("youtu.be/")
+        ? youtubeURL.split("youtu.be/")[1].split("?")[0]
+        : youtubeURL.split("v=")[1].split("&")[0];
+
+    document.getElementById("youtubePlayer").src =
+        `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+
+    document.getElementById("videoModal").style.display = "flex";
+});
+
+document.getElementById("fecharVideo").addEventListener("click", () => {
+    document.getElementById("videoModal").style.display = "none";
+    document.getElementById("youtubePlayer").src = "";
+});
